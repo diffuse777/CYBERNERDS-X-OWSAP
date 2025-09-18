4c730cc8c4e35965268e1c080c14f12630c1edd2
diffuse777 <iamkarthikeyen@gmail.com>
2025-09-18 17:42:19 +0530

    frrst

---
+++ ui.config.js
@@ -0,0 +1,8 @@
+// shadcn/ui config file
+module.exports = {
+  $schema: "https://ui.shadcn.com/schema.json",
+  style: "default",
+  themes: ["dark"],
+  tailwind: true,
+  components: ["button", "input", "card", "table", "toast"],
+};
